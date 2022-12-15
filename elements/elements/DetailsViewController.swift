bb6135b1ab3c881bf8f9405649a4dc8d86e29585
TypeCc <118025810+TypeCc@users.noreply.github.com>
2022-12-15 09:47:27 -0800

    my first table view project

---
+++ elements/elements/DetailsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  DetailsViewController.swift
+//  elements
+//
+//  Created by serif mete on 15.12.2022.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+            
+        }
+    
+    
+
+  
+
+}
